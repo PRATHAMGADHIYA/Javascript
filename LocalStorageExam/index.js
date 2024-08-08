7f08a7db91c07e68e365ac339a67107a7647aa42
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-08-08 22:34:15 +0530

    JS

--- LocalStorageExam/index.js
+++ LocalStorageExam/index.js
@@ -1,5 +1,18 @@
-window.localStorage.setItem('username','Pratham');
-window.localStorage.setItem('Email','Prathamgadhiya@gmail.com');
-window.localStorage.setItem('password','54321');
+const storeData = (e) => {
 
-console.log(localStorage);
+    e.preventDefault();
+
+    let name = document.getElementById('name').value;
+    let age = document.getElementById('age').value;
+    let email = document.getElementById('email').value;
+    localStorage.setItem('name', name);
+    localStorage.setItem('age', age);
+    localStorage.setItem('email', email);
+    displayData();
+    alert('Data stored successfully');
+    document.getElementById('name').value = '';
+    document.getElementById('age').value = '';
+    document.getElementById('email').value = '';
+}
+
+
