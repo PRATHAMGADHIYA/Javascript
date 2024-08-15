081f2d44494783e0494031ae1f1076462a4217c3
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-08-15 20:28:16 +0530

    JS

--- Digital Clock/index.js
+++ Digital Clock/index.js
@@ -1,27 +1,30 @@
-const clock=()=>{
-    let monthsnames=['January', 'February', 'March', 'April','May', 'Jun', 'July', 'August', 'September', 'octomber', 'november', 'december'];
-    let daysnames=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurseday', 'Friday', 'saterday'];
-    let today=new Date();
+setInterval(() => {
+    let monthsnames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let daysnames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-    document.getElementById("Date").innerHTML = (daysnames[today.getDay()]+""+ today.getDate()+""+monthsnames[today.getMonth()]+""+today.getFullYear());
+    let today = new Date();
 
-    let h=today.getHours();
-    let m=today.getMinutes();
-    let s=today.getSeconds();
+    document.getElementById("Date").innerHTML = daysnames[today.getDay()] + " " + today.getDate() + " " + monthsnames[today.getMonth()] + " " + today.getFullYear();
 
-    if (h<=11){
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+
+    if(h < 12){
         console.log("AM");
-    }
+    } 
     else{
         console.log("PM");
     }
-
-    h=h<12? '0'+h:h;
-    m=m<10? '0'+m:m;
-    s=s<10? '0'+s:s;
+    h = h % 12;
+    h = h
+    m = m
+    s = s
 
     document.getElementById("hour").innerHTML = h;
+
     document.getElementById("minute").innerHTML = m;
+
     document.getElementById("second").innerHTML = s;
-}
-var inter = setInterval(clock,400);
+    
+}, 1000);
