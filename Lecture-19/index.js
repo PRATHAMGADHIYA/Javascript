6edae6e72f89287bf74f9d0b531384efa570615b
PRATHAMGADHIYA <154453177+PRATHAMGADHIYA@users.noreply.github.com>
2024-08-20 11:01:06 +0530

    JS

--- Lecture-19/index.js
+++ Lecture-19/index.js
@@ -1,11 +1,11 @@
-const counters = document.querySelectorAll('.counter');
+const counters = document.querySelectorAll('.counter1');
 
 console.log(counters);
 
 counters.forEach(counter => {
     counter.innerHTML = '0';
 
-    let target = +counter.getAttribute('data-target');
+    let target = counter.getAttribute('data-target');
     console.log(target);
 
     let inc = target / 200;
@@ -13,7 +13,7 @@ counters.forEach(counter => {
 
     const incrementcounter = () => {
         if (count < target) {
-            count++;
+            count += 1;
             counter.innerText = count;
             setInterval(incrementcounter, 100)
         }
@@ -24,5 +24,88 @@ counters.forEach(counter => {
     }
     incrementcounter();
 
-})
+});
 
+const counters2 = document.querySelectorAll('.counter2');
+
+console.log(counters2);
+
+counters2.forEach(counter => {
+    counter.innerHTML = '0';
+
+    let target = counter.getAttribute('data-target');
+    console.log(target);
+
+    let inc = target / 100;
+    let count = 0
+
+    const incrementcounter = () => {
+        if (count < target) {
+            count += 1;
+            counter.innerText = count;
+            setInterval(incrementcounter, 300)
+        }
+        else {
+            clearInterval(interval);
+            counter.innerText = target;
+        }
+    }
+    incrementcounter();
+
+});
+
+const counters3 = document.querySelectorAll('.counter3');
+
+console.log(counters3);
+
+counters3.forEach(counter => {
+    counter.innerHTML = '0';
+
+    let target = counter.getAttribute('data-target');
+    console.log(target);
+
+    let inc = target / 100;
+    let count = 0
+
+    const incrementcounter = () => {
+        if (count < target) {
+            count += 1;
+            counter.innerText = count;
+            setInterval(incrementcounter, 400)
+        }
+        else {
+            clearInterval(interval);
+            counter.innerText = target;
+        }
+    }
+    incrementcounter();
+
+});
+
+const counters4 = document.querySelectorAll('.counter4');
+
+console.log(counters4);
+
+counters4.forEach(counter => {
+    counter.innerHTML = '0';
+
+    let target = counter.getAttribute('data-target');
+    console.log(target);
+
+    
+    let count = 0
+
+    const incrementcounter = () => {
+        if (count < target) {
+            count += 1;
+            counter.innerText = count;
+            setInterval(incrementcounter, 500)
+        }
+        else {
+            clearInterval(interval);
+            counter.innerText = target;
+        }
+    }
+    incrementcounter();
+
+});
