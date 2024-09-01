2fc1720eebb3b4e4e87539f6b58a3488db5b2f84
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-09-01 11:55:45 +0530

    JS

---
+++ Project-1/components/Navbar.js
@@ -0,0 +1,36 @@
+const Navbar = () => {
+  return`
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" href="/Project-1/index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/Project-1/pages/addProduct.html">Add Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/Project-1/pages/product.html">Products</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/Project-1/pages/cart.html">Cart</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/Project-1/pages/login.html">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/Project-1/pages/signup.html">signUp</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  
+  `
+}
+export default Navbar
--- Project-1/js/login.js
+++ Project-1/js/login.js
@@ -2,20 +2,24 @@ import getValue from "../components/helper.js";
 import Navbar from "../components/Navbar.js";
 
 document.getElementById("navbar").innerHTML = Navbar()
-let users = JSON.parse(localStorage.getItem('users')) || []
+
+let users = JSON.parse(localStorage.getItem('users')) || [];
 const handledata = (e) => {
     e.preventDefault();
 
     let user = {
-        username: getValue("#Username"),
         email: getValue("#Email"),
         password: getValue("#Password")
     };
-   users.push(user);
-    localStorage.setItem("users", JSON.stringify(users));
 
-    window.location.href = "/Project-1/index.html"
+    let isMatched = users.filter(
+        (ele) => ele.Email == user.Email && ele.Password == user.Password
 
-}
+    );
+
+    alert('Login Successfully');
+    window.location.href = "/Project-1/index.html";
+
+};
 
 document.querySelector('#userData').addEventListener('submit', handledata);
