737c5ada5bd78e8005dad3c294a84ca47bdcd6c5
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-09-16 19:42:14 +0530

    JS

---
+++ Exam-9/components/helper.js
@@ -0,0 +1,4 @@
+const getValue = (id) => {
+    return document.querySelector(id).value;
+};
+export default getValue;
---
+++ Exam-9/js/adddata.js
@@ -0,0 +1,4 @@
+import getValue from "../components/helper.js";
+import navbar from "../components/navbar.js";
+
+document.getElementById("id").innerHTML = navbar();
