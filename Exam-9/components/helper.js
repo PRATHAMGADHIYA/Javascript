a24d2cbb0ffd0df7309f8a8af160ebf9b74863f2
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-09-18 10:48:58 +0530

    JS

--- Exam-9/components/helper.js
+++ Exam-9/components/helper.js
@@ -2,3 +2,15 @@ const getValue = (id) => {
     return document.querySelector(id).value;
 };
 export default getValue;
+
+export const createTag = (tag, value) => {
+    let tagName = document.createElement(tag);  
+    if (tag === "img") {
+        tagName.src = value;
+    } else {
+        tagName.innerHTML = value;
+    }
+
+    return tagName;
+};
+
--- Exam-9/js/adddata.js
+++ Exam-9/js/adddata.js
@@ -1,4 +1,26 @@
 import getValue from "../components/helper.js";
 import navbar from "../components/navbar.js";
 
-document.getElementById("id").innerHTML = navbar();
+document.getElementById("navbar").innerHTML = navbar();
+
+let Data = JSON.parse(localStorage.getItem("Data")) || []
+
+const handedata = (e) => {
+    e.preventDefault();
+
+    let data = {
+        name:getValue("#name"),
+        age:getValue("#age"),
+        grid :getValue("#grid"),
+        email:getValue("#email"),
+        course:getValue("#course"),
+        fee:getValue("#fee")
+    }
+    Data.push(data);
+    localStorage.setItem("Data", JSON.stringify(Data));
+}
+
+
+
+document.getElementById("studentData").addEventListener("click", handedata);
+
