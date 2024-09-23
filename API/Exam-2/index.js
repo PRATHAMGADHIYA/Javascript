e0128648853324ab1edb3579efc03fe5cd5311be
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-09-23 21:50:59 +0530

    JS

--- API/Exam-2/index.js
+++ API/Exam-2/index.js
@@ -1,15 +1,23 @@
+let productsdata = [];
 
 const getData = async () => {
     let req = await fetch("https://dummyjson.com/products");
     let res = await req.json();
-    mapper(res)
+    productsdata = res.products; 
+    displayProducts(productsdata); 
 }
 
 getData();
 
-const mapper = (Data) => {
-    Data.products.map((data) => {
-        
+
+const displayProducts = (Data) => {
+    const productsList = document.getElementById("productsList");
+    productsList.innerHTML = ""; 
+
+    Data.map((data) => {
+        let productDiv = document.createElement("div");
+        productDiv.classList.add("product");
+
         let id = document.createElement("h2");
         id.innerHTML = data.id;
         let title = document.createElement("h2");
@@ -18,12 +26,44 @@ const mapper = (Data) => {
         price.innerHTML = data.price;
         let category = document.createElement("h5");
         category.innerHTML = data.category;
-        let rate = document.createElement("h5");
-        rate.innerHTML = data.rate;
+        let stars = document.createElement("h5");
+        stars.innerHTML = data.rating; 
         let image = document.createElement("img");
-        image.src = data.image;
+        image.src = data.thumbnail;
 
-        document.getElementById("productsList").append(id, title, price, category, rate, image);
+        productDiv.append(id, title, price, category, stars, image);
+        productsList.appendChild(productDiv);
     });
-}
+};
 
+document.getElementById("lth").addEventListener("click", () => {
+    let sorted = [...productsdata].sort((a, b) => a.price - b.price);
+    displayProducts(sorted);
+});
+
+document.getElementById("htl").addEventListener("click", () => {
+    let sorted = [...productsdata].sort((a, b) => b.price - a.price);
+    displayProducts(sorted);
+});
+
+document.getElementById("beauty").addEventListener("click", () => {
+    let filtering = productsdata.filter(product => product.category === "beauty");
+    displayProducts(filtering);
+});
+
+document.getElementById("fragrances").addEventListener("click", () => {
+    let filtering = productsdata.filter(product => product.category === "fragrances");
+    displayProducts(filtering);
+});
+
+document.getElementById("furniture").addEventListener("click", () => {
+    let filtering = productsdata.filter(product => product.category === "furniture");
+    displayProducts(filtering);
+});
+document.getElementById("search").addEventListener("keypress", (e) => {
+
+    if (e.key === "Enter") {
+
+    }
+
+});
