eb2b16359b7d020ba555ee39ca0ef6cf117e3a72
Pratham Gadhiya <prathamgadhiya@gmail.com>
2024-10-02 10:09:13 +0530

    JS

--- Final Exam/components/api.js
+++ Final Exam/components/api.js
@@ -1,6 +1,37 @@
 const methods={
     get: async ()=>{
-      let req= await fetch("https://localhost:3000/students");  
-      let res=req.json()
+      let req= await fetch("https://localhost:3000/Questions");  
+      let res=req.json();
+      return res;
     },
+    post: async(data)=>{
+        let req= await fetch("https://localhost:3000/Questions", {
+            method:'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        let res=await req.json();
+        return res;
+    },
+    patch: async(data)=>{
+        let req= await fetch(`https://localhost:3000/Questions/${data.id}`, {
+            method:'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        let res=await req.json();
+        return res;
+    },
+    delete: async(id)=>{
+        let req= await fetch(`https://localhost:3000/Questions/${id}`, {
+            method:'DELETE',
+        });
+        let res=await req.json();
+        return res;
+    }
 }
+export default methods;
--- Final Exam/index.js
+++ Final Exam/index.js
@@ -1,16 +1,53 @@
-let users=[];
+import methods from "./components/api.js";
 
-const handledata=(e)=>{
-     e.preventDefault();
+const handledata = async(e) => {
+    e.preventDefault();
 
-    let user={
-        question:document.getElementById('question').value,
-        option1:document.getElementById('option1').value,
-        option2:document.getElementById('option2').value,
-        option3:document.getElementById('option3').value,
-        option4:document.getElementById('option4').value,
-        answer:document.getElementById('answer').value
-    }    
+    let user = {
+        question: document.getElementById('question').value,
+        option1: document.getElementById('option1').value,
+        option2: document.getElementById('option2').value,
+        option3: document.getElementById('option3').value,
+        option4: document.getElementById('option4').value,
+        answer: document.getElementById('Answer').value
+    }
+    console.log(user);
+
+    await methods.post("user",user)
+
+    await uimaker(user);
 }
-document.getElementById('Answer').addEventListener('submit',handledata)
 
+document.getElementById('add-data').addEventListener('submit', handledata);
+
+const uimaker = async (data) => {
+
+    let Data= await methods.get();
+
+    data.map((ele) => {
+        let div = document.createElement('div');
+
+        let question = document.createElement('h2');
+        question.innerHTML = ele.question;
+
+        let option1 = document.createElement('button');
+        option1.innerHTML = ele.option1;
+
+        let option2 = document.createElement('button');
+        option2.innerHTML = ele.option2;
+
+        let option3 = document.createElement('button');
+        option3.innerHTML = ele.option3;
+
+        let option4 = document.createElement('button');
+        option4.innerHTML = ele.option4;
+
+        let answer = document.createElement('button');
+        answer.innerHTML = ele.answer;
+
+        div.append(question, option1, option2, option3, option4, answer);
+
+        document.getElementById('question-list').append(div);
+    });
+}
+uimaker();
